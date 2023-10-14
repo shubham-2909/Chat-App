1d7fd9066a6788646fea2b8c5462118460551052
sg976 <sg9769356@gmail.com>
2023-10-14 19:35:46 +0530

    some performace fixes

--- src/app/api/message/send/route.ts
+++ src/app/api/message/send/route.ts
@@ -44,24 +44,26 @@ export async function POST(req: Request) {
     const message = messageValidator.parse(messageData)
 
     // notify all connected chat room clients beforehand
-    await pusherServer.trigger(
-      toPusherKey(`chat:${chatId}`),
-      'incoming-message',
-      message
-    )
-    await pusherServer.trigger(
-      toPusherKey(`user:${friendId}:chats`),
-      'new_message',
-      {
-        ...message,
-        senderImg: sender.image,
-        senderName: sender.name,
-      }
-    )
-    await db.zadd(`chat:${chatId}:messages`, {
-      score: timestamp,
-      member: JSON.stringify(message),
-    })
+    await Promise.all([
+      pusherServer.trigger(
+        toPusherKey(`chat:${chatId}`),
+        'incoming-message',
+        message
+      ),
+      pusherServer.trigger(
+        toPusherKey(`user:${friendId}:chats`),
+        'new_message',
+        {
+          ...message,
+          senderImg: sender.image,
+          senderName: sender.name,
+        }
+      ),
+      db.zadd(`chat:${chatId}:messages`, {
+        score: timestamp,
+        member: JSON.stringify(message),
+      }),
+    ])
     return new Response('OK')
   } catch (error) {
     if (error instanceof Error) {
